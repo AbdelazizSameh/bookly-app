ea092b222e1718e2d711646f9b55830c4518e10d
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-10-08 15:44:33 +0300

    user Either class to make the functions make return of two types

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import '/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+import '/Features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchFeaturedBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
