06ac4e232ff159dfc52ba0202f8f686350bfe1fe
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-10-10 17:07:05 +0300

    setup Service Locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'core/utils/service_locator.dart';
+
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -12,18 +19,32 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.appRouter,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              FeaturedBooksCubit(getIt.get<HomeRepoImplementation>()),
+        ),
+        BlocProvider(
+          create: (context) =>
+              NewestBooksCubit(getIt.get<HomeRepoImplementation>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.appRouter,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(),
+        ),
+        darkTheme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
+        themeMode: ThemeMode.dark,
       ),
-      darkTheme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-      ),
-      themeMode: ThemeMode.dark,
     );
   }
 }
