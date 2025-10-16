c5166a86cfd7da3442bf6b4de675323096811c92
AbdelazizSameh <abdelzizsameh364@gmail.com>
2025-10-16 23:12:45 +0300

    add Skeletonizer to similar books list view

--- lib/Features/home/presentation/views/widgets/similar_books_section.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_section.dart
@@ -1,4 +1,7 @@
+import 'package:bookly_app/Features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import '../../../../../core/utils/styles.dart';
 import 'similar_books_list_view.dart';
 
@@ -18,7 +21,15 @@ class SimilarBooksSection extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 18),
-        const SimilarBooksListView(),
+        BlocSelector<SimilarBooksCubit, SimilarBooksState, bool>(
+          selector: (state) => state is SimilarBooksSuccess,
+          builder: (context, state) {
+            return Skeletonizer(
+              enabled: !state,
+              child: const SimilarBooksListView(),
+            );
+          },
+        ),
         const SizedBox(height: 16),
       ],
     );
